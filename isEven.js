39cb820d670ba570aeda1f4b5502b069f3960fbc
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-13 15:44:02 +0530

    isEven

---
+++ isEven.js
@@ -0,0 +1,15 @@
+/*
+Write a function called “isEven”.
+Given a number, “isEven” returns whether it is even.
+
+Input:
+  isEven(11);
+Output:
+  false
+
+*/
+
+function isEven(num)
+{
+  return num % 2 == 0;
+}
