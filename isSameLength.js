033a9f0c7e74f65bd4c905f609bf2c9e2070e7f3
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-13 16:29:46 +0530

    isSameLength

---
+++ isSameLength.js
@@ -0,0 +1,14 @@
+/*
+Write a function called “isSameLength”.
+Given two words, “isSameLength” returns whether the given words have the same length.
+
+Input:
+  isSameLength(“GUVI”, “GEEK”);
+Output:
+  true
+*/
+
+function isSameLength(str1, str2)
+{
+  return str1.length == str2.length;
+}
