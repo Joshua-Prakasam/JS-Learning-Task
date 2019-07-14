7b744bd5c2b734d994091b5477885eca30cd0d8e
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-07-14 12:29:10 +0530

    Init

---
+++ JSON-List-Workouts/printAllValues.js
@@ -0,0 +1,22 @@
+/*
+Parsing an JSON object’s Values:
+
+Write a function called “printAllValues” which returns an newArray of all the input object’s values.
+
+Input (Object):
+  var object = {name: “RajiniKanth”, age: 33, hasPets : false};
+
+Output:
+  [“RajiniKanth”, 33, false]
+
+Sample Function proto:
+  function printAllValues(obj) {
+  // your code here
+  }
+
+*/
+
+function printAllValues(obj)
+{
+  
+}
