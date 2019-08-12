fc2b19e856c79ccd38820580c4738059aa49de9f
Joshua-Prakasam <40373946+Joshua-Prakasam@users.noreply.github.com>
2019-08-12 21:59:27 +0530

    Init

---
+++ Question.js
@@ -0,0 +1,35 @@
+//If anyone one wants sample data, take it here.
+
+var arr = [
+{name: Ellis, 		age: 26},
+{name: Higgins, 	age: 25},
+{name: Fenton, 		age: 29},
+{name: Hamilton,	age: 28},
+{name: Sullivan,	age: 28},
+{name: Riley,		age: 25},
+{name: Armstrong,	age: 21},
+{name: Bailey,		age: 23},
+{name: Martin,		age: 23},
+{name: Reed,		age: 26},
+{name: Daryl,	  	age: 23},
+{name: Sam, 		age: 20},
+{name: Mason,	 	aIge: 22},
+{name: Oliver, 		age: 26},
+{name: Douglas, 	age: 19},
+{name: Miller, 		age: 20},
+{name: Riley,	 	age: 28},
+{name: Smith 		age: 24},
+{name: Brianna,		age: 25},
+{name: James 		age: 25},
+{name: Adam 		age: 28},
+{name: Morrison,	age: 25},
+{name: Jared,		age: 26},
+{name: Russell,		age: 27},
+{name: Camila 		age: 28},
+{name: Freddie, 	age: 28},
+{name: Robinson, 	age: 24},
+{name: Stewart, 	age: 30},
+{name: Montgomery,	age: 21},
+{name: Harris, 		age: 23}
+];
+
